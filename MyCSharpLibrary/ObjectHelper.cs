28aac95c2488caeb0894478f74aced34d899aea2
320陳永修 <peterch@mirle.com.tw>
2018-07-30 21:11:27 +0800

    Add project files.

---
+++ MyCSharpLibrary/ObjectHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MyCSharpLibrary
+{
+    public static class ObjectHelper
+    {
+        /// <summary>
+        /// 深複製物件
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static T DeepClone<T>(this T obj)
+        {
+            using (var stream = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(stream, obj);
+                stream.Position = 0;
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
